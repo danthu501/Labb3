51842f71db381b7d58af6c478cb3e1e32db141dd
danthu501 <91268795+danthu501@users.noreply.github.com>
2022-02-01 11:30:26 +0100

    Added Console Readkey in show all students in one class

--- Labb3/Program.cs
+++ Labb3/Program.cs
@@ -112,6 +112,7 @@ namespace Labb3
                             }
 
                         }
+                        Console.ReadKey();
                         
                         break;
                        
